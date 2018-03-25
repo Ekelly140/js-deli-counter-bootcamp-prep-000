9669dcb02c19d127a8df7f16fb0c00ff50165312
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 20:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name){
   var line = katzDeliLine.length + 1
   var lineOrder = []
-  for (var i = 0; i <= line; i++){
-    var place = i+1
+  var place = i+1
     lineOrder.push(name)
   }
   return ("Welcome, " +name + ". You are number " + place + " in line.")
